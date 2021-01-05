fb6c76a4f176dc44566255300e5260bd6da8e24a
BAH <marouwanebah@gmail.com>
2021-01-05 16:46:54 +0100

    Read fix

--- App/main.py
+++ App/main.py
@@ -4,14 +4,11 @@ import openpyxl
 #climat = pd.read_csv("../data/Climat.xlsx")
 
 def read_exel(path, sheet):
-    wb = openpyxl.load_workbook(path)
-    sheets = wb.sheetnames
-    ws = wb[sheets[sheet]]
-    #result = pd.read_excel(path, sheet_name=None, engine='openpyxl')
-    #print(result)
-    #return result
-    print(ws)
-    return ws
+    xls = pd.ExcelFile(path)
+    xls.sheet_names[sheet]
+    result = pd.read_excel(path, sheet_name=xls.sheet_names[sheet], engine='openpyxl')
+    print(result)
+    return result
 
 
 if __name__ == '__main__':
