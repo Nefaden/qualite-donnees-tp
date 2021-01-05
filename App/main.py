4940b1a827764f32778f807c6ba5d82e4a09ebe5
BAH <marouwanebah@gmail.com>
2021-01-05 16:23:10 +0100

    Add function read

--- App/main.py
+++ App/main.py
@@ -1,3 +1,12 @@
 import pandas as pd
 
-climat = pd.read_csv("../data/Climat.xlsx")
+#climat = pd.read_csv("../data/Climat.xlsx")
+
+def read_exel(path, sheet):
+    result = pd.read_excel(path, sheet_name=None, engine='openpyxl')
+    print(result)
+    return result
+
+
+if __name__ == '__main__':
+    read_exel('../Data/Climat.xlsx', 'SI')
