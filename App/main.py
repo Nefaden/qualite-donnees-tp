a1fc47f9a85c53fd604e58ec7dc6acd87038e3ab
Yann Durand <ydurand@Yanns-MacBook-Pro.local>
2021-01-08 17:30:28 +0100

    excel to csv

--- App/main.py
+++ App/main.py
@@ -3,8 +3,6 @@ import openpyxl
 import matplotlib.pyplot as plt
 import numpy as np
 
-#climat = pd.read_csv("../data/Climat.xlsx")
-
 def read_exel(path, sheet):
     xls = pd.ExcelFile(path)
     xls.sheet_names[sheet]
@@ -12,7 +10,24 @@ def read_exel(path, sheet):
     print(result)
     return result
 
+def excelToCsv(sheet):
+    csvfile = 'Climat.csv'
+    result = sheet.to_csv('../Data/' + csvfile, encoding='utf-8', index=False)
+    return(result)
+
+def getDataFrame(csv):
+    df = pd.DataFrame(csv)
+    print(df.dtypes)
+    getPlotFromDataFrame(df)
+
+def getPlotFromDataFrame(df):
+    df1 = pd.DataFrame(df, columns=['Jour','Température'])
+    plot = df.plot(x ='Jour', y='Température', kind = 'line')
+    print(plot)
 
 if __name__ == '__main__':
-   result = read_exel('../Data/Climat.xlsx', 0)
+   excelfile = read_exel('../Data/Climat.xlsx', 0)
+   excelToCsv(excelfile)
+   csvfile = pd.read_csv('../Data/Climat 2.csv')
+   getDataFrame(csvfile)
    
