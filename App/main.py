f452911079592670588c79ad32576e6d5fbd76c1
BAH <marouwanebah@gmail.com>
2021-01-08 17:07:31 +0100

    fix header

--- App/main.py
+++ App/main.py
@@ -3,10 +3,11 @@ import openpyxl
 import matplotlib.pyplot as plt
 import numpy as np
 
-def read_exel(path, sheet):
+def read_exel(path, sheet , collones):
     xls = pd.ExcelFile(path)
     xls.sheet_names[sheet]
-    result = pd.read_excel(path, sheet_name=xls.sheet_names[sheet], engine='openpyxl')
+    result = pd.read_excel(path, sheet_name=xls.sheet_names[sheet],  header=3, usecols=collones, engine='openpyxl')
+    result = result
     print(result)
     return result
 
@@ -21,10 +22,20 @@ def getDataFrame(csv):
 
 ##def getPlotFromDataFrame(df):
 
+def calcul_moyenne(datframe):
+    print(datframe.describe(include="all"))
+    #Remplacer les Nan par 0
+    result = datframe.fillna(0)
+    result = datframe.mean(axis=0)
+    print(result)
 
 if __name__ == '__main__':
-   excelfile = read_exel('../Data/Climat.xlsx', 0)
-   excelToCsv(excelfile)
-   csvfile = pd.read_csv('../Data/Climat.csv')
-   getDataFrame(csvfile)
+   result = read_exel('../Data/Climat.xlsx', 1, "C:O")
+
+   calcul_moyenne(result)
+
+   #excelfile = read_exel('../Data/Climat.xlsx', 0)
+   #excelToCsv(excelfile)
+   #csvfile = pd.read_csv('../Data/Climat.csv')
+   #getDataFrame(csvfile)
    
