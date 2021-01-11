b1279b3d65cafce2ddc5333ef3e75571425c1e43
Yann Durand <ydurand@Yanns-MacBook-Pro.local>
2021-01-11 16:16:00 +0100

    fix index

--- App/main.py
+++ App/main.py
@@ -3,12 +3,11 @@ import openpyxl
 import matplotlib.pyplot as plt
 import numpy as np
 
-def read_exel(path, sheet , collones):
+def read_excel(path, sheet , colonnes):
     xls = pd.ExcelFile(path)
     xls.sheet_names[sheet]
-    result = pd.read_excel(path, sheet_name=xls.sheet_names[sheet],  header=3, usecols=collones, engine='openpyxl')
-    result = result
-    print(result)
+    result = pd.read_excel(path, sheet_name=xls.sheet_names[sheet], index_col=0, header=2, usecols=colonnes, engine='openpyxl')
+    getPlotFromDataFrame(result)
     return result
 
 def excelToCsv(sheet):
@@ -22,19 +21,25 @@ def getDataFrame(csv):
 
 ##def getPlotFromDataFrame(df):
 
-def calcul_moyenne(datframe):
-    print(datframe.describe(include="all"))
+def calcul_moyenne(dataframe):
+    print(dataframe.describe(include="all"))
     #Remplacer les Nan par 0
-    result = datframe.fillna(0)
-    result = datframe.mean(axis=0)
+    result = dataframe.fillna(0)
+    result = dataframe.mean(axis=0)
     print(result)
 
+def getPlotFromDataFrame(df):
+    df1 = pd.DataFrame(df, columns=['Janvier'])
+    print(df1)
+    df1.plot(x ='Jour', y='Température', kind = 'line')
+    plt.show()
+
 if __name__ == '__main__':
-   result = read_exel('../Data/Climat.xlsx', 1, "C:O")
+   result = read_excel('../Data/Climat.xlsx', 0, "C:O")
 
    calcul_moyenne(result)
 
-   #excelfile = read_exel('../Data/Climat.xlsx', 0)
+   #excelfile = read_excel('../Data/Climat.xlsx', 0)
    #excelToCsv(excelfile)
    #csvfile = pd.read_csv('../Data/Climat.csv')
    #getDataFrame(csvfile)
