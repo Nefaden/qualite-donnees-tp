89759dbba34b10ebc152ea80df96db5b88e41e1e
Yann Durand <ydurand@Yanns-MacBook-Pro.local>
2021-01-12 11:31:52 +0100

    changing data

--- App/main.py
+++ App/main.py
@@ -2,32 +2,18 @@ import pandas as pd
 import openpyxl
 import matplotlib.pyplot as plt
 import numpy as np
+from numpy import genfromtxt, nanmean, isnan, nanstd, nanmin, nanmax
 
-def read_exel(path, sheet):
-    xls = pd.ExcelFile(path)
-    xls.sheet_names[sheet]
-    result = pd.read_excel(path, sheet_name=xls.sheet_names[sheet], engine='openpyxl')
-    print(result)
-    return result
+csvfile = pd.read_csv('../Data/Climat.csv')
+getPlotFromDataFrame(csvfile)
 
-def excelToCsv(sheet):
-    csvfile = 'Climat.csv'
-    result = sheet.to_csv('../Data/' + csvfile, encoding='utf-8', index=False)
-    return(result)
+months = ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre"]
 
-def getDataFrame(csv):
-    df = pd.DataFrame(csv)
-    print(df.dtypes)
-    getPlotFromDataFrame(df)
-
-def getPlotFromDataFrame(df):
-    df1 = pd.DataFrame(df, columns=['Jour','Température'])
-    plot = df.plot(x ='Jour', y='Température', kind = 'line')
-    print(plot)
-
-if __name__ == '__main__':
-   excelfile = read_exel('../Data/Climat.xlsx', 0)
-   excelToCsv(excelfile)
-   csvfile = pd.read_csv('../Data/Climat 2.csv')
-   getDataFrame(csvfile)
-   
+def getPlotFromDataFrame(csvfile):
+    df = pd.DataFrame(csvfile, columns=['Jour','Température'])
+    print(df)
+    df.index.name='Jour'
+    df['Jour']=df.index
+    df = df.reset_index(drop=True)
+    df.plot(x ='Jour', y='Température', kind = 'line')
+    #plt.show()
